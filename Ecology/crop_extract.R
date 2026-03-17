7ceb26c1885c916150511edd8b239255cc4a2f93
Qian Tang <39941019+qt37t247@users.noreply.github.com>
2026-03-17 13:52:10 -0400

    Crop, re-rasterize and extract data
    From data not available in Google Earth Engine

--- Ecology/crop_extract.R
+++ Ecology/crop_extract.R
@@ -1,12 +1,25 @@
 library(raster)
 
+list.filenames <- list.files(pattern="\\.nc$")
+
+ext <- extent(-76, -74, 38, 42)
+
+for (i in list.filenames){
+  
+  writeRaster(crop(projectRaster(raster(i), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"), ext), paste(i,"_cropped"), format="GTiff", overwrite=TRUE)
+
+} 
+
+check <- raster(list.files(pattern="\\.tif _cropped.tif$")[1])
+
+plot(check)
+
+
 list.filenames <- list.files(pattern="\\ _cropped.tif$")
 
 allrasters <- stack(list.filenames)
 
 # rerasterize
-
-ext <- extent(-76, -74, 38, 42)
 
 empty <- raster("prism_ppt_2020.tif")
 empty <- crop(projectRaster(empty, crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"), ext)
